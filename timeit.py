2e0311be733579121f0050dc4a2c879833457a53
CatHer431 <catherine.hernandez@sjsu.edu>
2022-03-10 23:13:23 -0800

    Updated timeit.py

--- timeit.py
+++ timeit.py
@@ -4,8 +4,7 @@ def calculate_time(func):
   def wrapper():
     start = time.time()
     func()
-    finish  = time.time()
-    runTime = finish  - start 
+    runTime = time.time() - start 
     print("Run Time: " + str(runTime))
   return wrapper
 
