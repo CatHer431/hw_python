3e4d0b9c5d13460d0f2bdf809c49bb2a4fb180d8
CatHer431 <catherine.hernandez@sjsu.edu>
2022-03-10 23:48:08 -0800

    question 5

---
+++ tracker.py
@@ -0,0 +1,16 @@
+def func_counter(func):
+ def wrapper(x):
+  wrapper.counter = wrapper.counter + 1
+  func(x)
+ wrapper.counter = 0
+ return wrapper
+
+
+
+@func_counter
+def foo(y):
+    return y+2**3-34
+
+foo(3)
+foo(7)
+print(foo.counter) # expect 2 as output
